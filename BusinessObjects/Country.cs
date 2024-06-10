112c5b34532b8e6caa87e5ee53bf8239a499a3ff
nminh220704 <147362409+nminh220704@users.noreply.github.com>
2024-06-10 17:07:37 +0700

    Create Country.cs

---
+++ BusinessObjects/Country.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Country
+{	
+	public int countryID {  get; set; }
+	public string countryName { get; set; }
+	public int RegionID { get; set; }
+
+	public Country(int countryID, string countryName, int regionID)
+	{
+		this.countryID = countryID;
+		this.countryName = countryName;
+		this.regionID = regionID;
+	}
+}
